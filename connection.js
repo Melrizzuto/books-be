10b949afdd7a86f60cb749caa0657f5dc8d34e02
Melrizzuto <melrizzuto4@gmail.com>
2025-07-04 19:26:55 +0200

    fix: added a db online

--- connection.js
+++ connection.js
@@ -1,10 +1,11 @@
 import mysql from "mysql2";
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'ciaociaociao!',
-    database: 'db_book',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 connection.connect((err) => {
